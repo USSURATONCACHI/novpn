3b7549e33e31a5bc5a7efc3b3914aea0fcf4b795
Redchin Daniil <redchindaniil@gmail.com>
2024-11-01 01:46:38 +0700

    Bugs for 1.1.0 fixed

--- nu_wait_for_device.sh
+++ nu_wait_for_device.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-source novpn_ns params
+DEV=$(cat /etc/novpn | grep DEV | awk '{print $2}')
 
 until ip link show $DEV | grep "UP"; do 
     echo "Waiting for device $DEV to be UP..."
     sleep 1; 
 done
-echo "Device $DEV is finally up."                                                       
+echo "Device $DEV is finally up."
